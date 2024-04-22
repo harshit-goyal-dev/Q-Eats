e4ac1e7312989b6d1b0522fab8b242b72167210d
CrioUser <crio-user@criodo.com>
2024-04-22 13:45:37 +0530

    ME_QEATS_V2_MODULE_SERIALIZATION

--- qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -13,7 +13,9 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 // TODO: CRIO_TASK_MODULE_SERIALIZATION
 //  Implement Restaurant class.
@@ -32,8 +34,21 @@ import lombok.NoArgsConstructor;
 //    "South Indian"
 //  ]
 // }
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Restaurant {
+    private String restaurantId;
+    private String name;
+    private String city;
+    private String imageUrl;
+    private double latitude;
+    private double longitude;
+    private String opensAt;
+    private String closesAt;
+    private String[] attributes;
 
+   
 }
 
